dd43118670bc6475be52cd5515d3c8a58149e257
at-tinhnguyen <tinh.nguyen@asiantech.vn>
2017-12-16 10:35:33 +0700

    add reporting

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'report', 'board'],
 
     # always loaded
     'data': [
@@ -29,6 +29,8 @@
         'views/openacademy.xml',
         'views/partner.xml',
         'views/session_workflow.xml',
+        'views/session_board.xml',
+        'reports.xml',
 
     ],
     # only loaded in demonstration mode
